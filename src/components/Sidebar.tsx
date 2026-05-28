1850bc3cdb86446f8a555eb50c833c0771b65c1d
Ismail H Hridoy <92619410+ismailhhridoy@users.noreply.github.com>
2026-05-29 04:59:38 +0600

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -28,12 +28,12 @@ export function Sidebar({ activeTab, setActiveTab, onLoginClick }: SidebarProps)
   return (
     <aside className="hidden lg:flex lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-64 lg:flex-col lg:bg-emerald-950 lg:text-white">
       {/* Logo */}
-<div className="px-4 py-4 border-b border-emerald-900/60">
-  <div className="bg-white rounded-xl px-3 py-2">
+<div className="px-4 py-4 ">
+  <div className="bg-white rounded-xl px-5 py-1">
     <img
       src="/icons/CareAid-Ai-Logo.png"
       alt="CareAid AI"
-      className="h-12 w-auto object-contain"
+      className="h-13 w-auto object-contain"
     />
   </div>
   <p className="text-[11px] text-emerald-400 font-medium mt-2 pl-1">
